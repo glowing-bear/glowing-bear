befe6e9160fd86cf497276ab351c3202f307cd9b
Tor Hveem <tor@hveem.no>
2013-10-07 00:59:40 +0200

    Use target blank on links so users dont navigate away from app

--- js/websockets.js
+++ js/websockets.js
@@ -235,7 +235,7 @@ weechat.factory('urlPlugin', [function() {
         var urlPattern = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
         var url = message.match(urlPattern);
         if (url) {
-            return '<a href="' + url[0] + '">' + message + '</a>';
+            return '<a target="_blank" href="' + url[0] + '">' + message + '</a>';
         }
         return null;
     }
