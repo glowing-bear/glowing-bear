7db8e31a7fc16cf7a97e51a13046b270c36bb15d
David Cormier <david.cormier@savoirfairelinux.com>
2013-10-08 17:06:47 -0400

    plugins: Move plugins to external module

--- js/websockets.js
+++ js/websockets.js
@@ -1,4 +1,4 @@
-var weechat = angular.module('weechat', ['localStorage', 'weechatModels']);
+var weechat = angular.module('weechat', ['localStorage', 'weechatModels', 'plugins']);
 
 weechat.factory('colors', [function($scope) {
 
@@ -109,87 +109,6 @@ weechat.factory('colors', [function($scope) {
 
 }]);
 
-weechat.factory('pluginManager', ['youtubePlugin', 'urlPlugin', 'imagePlugin', function(youtubePlugin, urlPlugin, imagePlugin) {
-
-    var plugins = [youtubePlugin, urlPlugin, imagePlugin]
-
-    var hookPlugin = function(plugin) {
-        plugins.push(plugin);
-    }
-
-    var contentForMessage = function(message) {
-
-        var content = [];
-        for (var i = 0; i < plugins.length; i++) {
-            var pluginContent = plugins[i].contentForMessage(message);
-            if (pluginContent) {
-                var pluginContent = {'visible': false, 'content': pluginContent }
-                content.push(pluginContent);
-
-                if (plugins[i].exclusive) {
-                    break;
-                }
-            }
-        }
-
-        return content;
-    }
-
-    return {
-        hookPlugin: hookPlugin,
-        contentForMessage: contentForMessage
-    }
-
-}]);
-
-weechat.factory('youtubePlugin', [function() {
-
-    var contentForMessage = function(message) {
-        if (message.indexOf('youtube.com') != -1) {
-            var index = message.indexOf("?v=");
-            var token = message.substr(index+3);
-            return '<iframe width="560" height="315" src="http://www.youtube.com/embed/' + token + '" frameborder="0" allowfullscreen></iframe>'
-        }
-        return null;
-    }
-
-    return {
-        contentForMessage: contentForMessage,
-        exclusive: true
-    }
-
-}]);
-
-weechat.factory('urlPlugin', [function() {
-    var contentForMessage = function(message) {
-        var urlPattern = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
-        var url = message.match(urlPattern);
-        if (url) {
-            return '<a href="' + url[0] + '">' + message + '</a>';
-        }
-        return null;
-    }
-
-    return {
-        contentForMessage: contentForMessage,
-        exclusive: false
-    }
-}]);
-
-weechat.factory('imagePlugin', [function() {
-    var contentForMessage = function(message) {
-		var urls = message.match(/https?:\/\/[^\s]*\.(jpg|png|gif)\b/)
-		if (urls != null) {
-			var url = urls[0]; /* Actually parse one url per message */
-			return '<img src="' + url + '" height="300">';
-        }
-        return null;
-    }
-
-    return {
-        contentForMessage: contentForMessage
-    }
-}]);
 
 weechat.factory('handlers', ['$rootScope', 'colors', 'models', 'pluginManager', function($rootScope, colors, models, pluginManager) {
 
