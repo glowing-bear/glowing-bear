0222dc102685615f3c4200d51a688a9caa5d5ec6
Tor Hveem <tor@hveem.no>
2013-10-16 23:04:42 +0200

    Only search buffer name

--- js/websockets.js
+++ js/websockets.js
@@ -386,7 +386,9 @@ weechat.controller('WeechatCtrl', ['$rootScope', '$scope', '$store', '$timeout',
 
     $scope.hasUnread = function(buffer) {
       // if search is set, return every buffer 
-      if($scope.search != undefined && $scope.search.$) return true;
+      if($scope.search && $scope.search != "") {
+          return true;
+      }
       if($scope.onlyUnread) {
         // Always show current buffer in list
         if (models.getActiveBuffer() == buffer) {
