2452858853cc35bf4abd5824445e1f2440ee4d6b
Tor Hveem <tor@hveem.no>
2014-02-19 11:28:48 +0100

    Fix globals. Fix init

--- js/models.js
+++ js/models.js
@@ -328,10 +328,8 @@ models.service('models', ['$rootScope', '$filter', function($rootScope, $filter)
 
 
     var BufferList = [];
-    activeBuffer = null;
-    previousBuffer = null;
-    unreads = 0;
-    notifications = 0;
+    var activeBuffer = null;
+    var previousBuffer = null;
 
     this.model = { 'buffers': {} };
 
@@ -344,7 +342,7 @@ models.service('models', ['$rootScope', '$filter', function($rootScope, $filter)
     this.addBuffer = function(buffer) {
         BufferList[buffer.id] = buffer;
         if (BufferList.length === 1) {
-            activeBuffer = buffer.id;
+            activeBuffer = buffer;
         }
         this.model.buffers[buffer.id] = buffer;
     };
