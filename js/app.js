24485de8376e9fe76791e27e356aef6873216dc0
Lorenz Hübschle-Schneider <lorenz-dev@lgh-alumni.de>
2014-06-07 19:20:41 +0100

    Add support for FirefoxOS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 document.addEventListener('deviceready', function() {
   navigator.splashscreen.hide();
 }, false);
+
+// FirefoxOS needs this for :active CSS rules (since it ignores :hover anyway)
+document.addEventListener("touchstart", function(){}, true);
