38f5ba7e262c16abdfe7fa4f289b2468fdc50e43
Lorenz Hübschle-Schneider <lorenz-dev@lgh-alumni.de>
2014-06-22 13:27:03 +0100

    Remove firefox app installation

--- js/glowingbear.js
+++ js/glowingbear.js
@@ -53,23 +53,6 @@ weechat.controller('WeechatCtrl', ['$rootScope', '$scope', '$store', '$timeout',
         $log.debug($rootScope.$$watchersCount);
     };
 
-    $scope.isinstalled = (function() {
-        // Check for firefox & app installed
-        if (navigator.mozApps !== undefined) {
-            navigator.mozApps.getSelf().onsuccess = function _onAppReady(evt) {
-                var app = evt.target.result;
-                if (app) {
-                    return true;
-                } else {
-                    return false;
-                }
-            };
-        } else {
-            return false;
-        }
-    }());
-
-
     // Detect page visibility attributes
     (function() {
         // Sadly, the page visibility API still has a lot of vendor prefixes
@@ -611,30 +594,6 @@ weechat.controller('WeechatCtrl', ['$rootScope', '$scope', '$store', '$timeout',
     $scope.reconnect = function() {
         var bufferId = models.getActiveBuffer().id;
         connection.attemptReconnect(bufferId, 3000);
-    };
-
-//XXX this is a bit out of place here, either move up to the rest of the firefox install code or remove
-    $scope.install = function() {
-        if (navigator.mozApps !== undefined) {
-            // Find absolute url with trailing '/' or '/index.html' removed
-            var base_url = location.protocol + '//' + location.host +
-                location.pathname.replace(/\/(index\.html)?$/, '');
-            var request = navigator.mozApps.install(base_url + '/manifest.webapp');
-            request.onsuccess = function () {
-                $scope.isinstalled = true;
-                // Save the App object that is returned
-                var appRecord = this.result;
-                // Start the app.
-                appRecord.launch();
-                alert('Installation successful!');
-            };
-            request.onerror = function () {
-                // Display the error information from the DOMError object
-                alert('Install failed, error: ' + this.error.name);
-            };
-        } else {
-            alert('Sorry. Only supported in Firefox v26+');
-        }
     };
 
     $scope.showModal = function(elementId) {
