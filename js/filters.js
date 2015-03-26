9b2a527cb7713f739e043624f47686c86e422c0c
Tor Hveem <tor@hveem.no>
2015-03-26 14:41:19 +0100

    Limit prefix parts to at most 25 chars

--- js/filters.js
+++ js/filters.js
@@ -167,4 +167,16 @@ weechat.filter('mathjax', function() {
     };
 });
 
+weechat.filter('prefixlimit', function() {
+    return function(input, chars) {
+        if (isNaN(chars)) return input;
+        if (chars <= 0) return '';
+        if (input && input.length > chars) {
+            input = input.substring(0, chars);
+            return input + '+';
+        }
+        return input;
+    };
+});
+
 })();
