4ce499c9ccd33cc4a34b2d93ca91f13641a2c3c5
Kasper Vaessen <kasper.vaessen@oulu.fi>
2016-10-13 21:37:00 +0300

    Base16 theme bugfixes and improvements

--- js/glowingbear.js
+++ js/glowingbear.js
@@ -21,7 +21,7 @@ weechat.controller('WeechatCtrl', ['$rootScope', '$scope', '$store', '$timeout',
     };
 
     $scope.command = '';
-    $scope.themes = ['dark', 'light', 'black', 'dark-spacious', 'blue', 'base16-default', 'base16-light', 'base16-mocha', 'base16-mocha-light', 'base16-solarized-dark', 'base16-solarized-light'];
+    $scope.themes = ['dark', 'light', 'black', 'dark-spacious', 'blue', 'base16-default', 'base16-light', 'base16-mocha', 'base16-solarized-dark', 'base16-solarized-light'];
 
     // Initialise all our settings, this needs to include all settings
     // or else they won't be saved to the localStorage.
