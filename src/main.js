dbfde3658d4c85046c524b8a6fbe49692b56a8f6
Rainer Hihn <hihn.rainer@gmail.com>
2021-04-13 18:53:46 +0200

    Removed old code

--- src/main.js
+++ src/main.js
@@ -6,12 +6,6 @@ import "angular-route";
 import "angular-sanitize";
 import "angular-touch";
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/fonts/glyphicons-halflings-regular.woff2';
-// import 'emojione/lib/js/emojione.min.js';
-// import 'linkifyjs/dist/linkify.min.js';
-// import 'linkifyjs/dist/linkify-string.min.js';
-
 import "./js/localstorage.js";
 import "./js/irc-utils.js";
 import "./js/bufferResume.js";
